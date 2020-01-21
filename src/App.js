52ed06d8f8e4d43dafb2516196e0d9358eb5ceec
Felipe <Felipeesousa.92@gmail.com>
2020-01-21 20:46:32 -0200

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import './app.css';
+import './soudebar.css';
+import './main.css';
+
+
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div id="app">
+     <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <div className="input-block">
+          <label htmlFor="github_username">Úsuario do github</label>
+          <input name="github_username" id="github_username" required></input>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" required></input>
+        </div>
+        <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input name="latitude" id="latitude" required></input>
+        </div>
+        <div className="input-block">
+          <label htmlFor="Longitude">Longitude</label>
+          <input name="Longitude" id="Longitude" required></input>
+        </div>
+        </div>
+            <button type="submit">Salvar</button>
+      </form>
+     </aside>
+     <main>
+       <ul>
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47892105?s=460&v=4" alt="Felipebeca"/>
+              <div className="user-info">
+                  <strong>Felipe</strong>
+                  <span>Reac.js, Node.js</span>
+              </div>
+            </header>
+            <p>Felipe de Sousa Alves</p>
+            <a href="https://github.com/Felipebeca">Acessar perfil no github</a>
+         </li>
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47892105?s=460&v=4" alt="Felipebeca"/>
+              <div className="user-info">
+                  <strong>Felipe</strong>
+                  <span>Reac.js, Node.js</span>
+              </div>
+            </header>
+            <p>Felipe de Sousa Alves</p>
+            <a href="https://github.com/Felipebeca">Acessar perfil no github</a>
+         </li>
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47892105?s=460&v=4" alt="Felipebeca"/>
+              <div className="user-info">
+                  <strong>Felipe</strong>
+                  <span>Reac.js, Node.js</span>
+              </div>
+            </header>
+            <p>Felipe de Sousa Alves</p>
+            <a href="https://github.com/Felipebeca">Acessar perfil no github</a>
+         </li>
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47892105?s=460&v=4" alt="Felipebeca"/>
+              <div className="user-info">
+                  <strong>Felipe</strong>
+                  <span>Reac.js, Node.js</span>
+              </div>
+            </header>
+            <p>Felipe de Sousa Alves</p>
+            <a href="https://github.com/Felipebeca">Acessar perfil no github</a>
+         </li>
+       </ul>
+     </main>
+
+   </div>
   );
 }
 
